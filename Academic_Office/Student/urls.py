c9699a063f1645b41e5c4583a7a82f5e2ea6317b
MDfarazuddin <farazuddin.m17@iiits.in>
2018-11-18 02:49:31 +0530

    added view courses for student view

--- Academic_Office/Student/urls.py
+++ Academic_Office/Student/urls.py
@@ -2,7 +2,7 @@ from django.urls import path ,include
 from . import views
 urlpatterns = [
 	path('profile',views.Student_profile,),
-	path('profile/courses/',views.Student_courses,name="student_courses"),
+	path('courses/<slug:slug>',views.Student_courses,name="student_courses"),
 	path('profile/grade-sheet',views.Student_grade_sheet,name="student_grades"),
     path('<slug:slug>',views.Student_profile),
 ]
--- Academic_Office/Student/views.py
+++ Academic_Office/Student/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Students 
+from .models import Students
 # Create your views here.
 def Student_profile(request,slug):
 	a_student = Students.objects.get(slug=slug)
 	return render(request,'Student/Student_View.html',{"a_student":a_student})
 	# return HttpResponse(slug)
 
-def Student_courses(request):
-	# a_student = Students.objects.get(slug=slug)
-	# return HttpResponse(slug2)
-	return render(request,'Student/Student_View_Courses.html')
+def Student_courses(request,slug):
+	a_student = Students.objects.get(slug=slug)
+	# return HttpResponse(slug)
+	return render(request,'Student/Student_View_Courses.html',{"a_student":a_student})
 
 def Student_grade_sheet(request):
 	return render(request,'Student/Student_View_Grade_Sheet.html')
