88ba60b184dbefb8c2b3f5d752b6cd839b02e16f
MDfarazuddin <farazuddin.m17@iiits.in>
2018-11-19 19:19:39 +0530

    fixed the bug by akshay

--- Academic_Office/Teacher/models.py
+++ Academic_Office/Teacher/models.py
@@ -4,7 +4,7 @@ from Student.models import Courses
 class Teachers(models.Model):
 	T_id = models.CharField(max_length = 15)
 	T_name = models.CharField(max_length = 20)
-	T_course_id = models.ForeignKey(Courses,on_delete=models.CASCADE)
+	T_course_id = models.ForeignKey(Courses,on_delete=None)
 	slug = models.SlugField()
 	T_email = models.EmailField(default="farazuddin.m17@iiits.in",blank=True)
 	T_password=models.CharField(max_length=50)
